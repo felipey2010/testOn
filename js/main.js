1af1760e532478e0e10466187407e4b87065ce24
PHILIP MAHAMA AKPANYI <mahamaphilip@yahoo.com>
2019-09-19 04:14:03 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+/*$('.main-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3
+});*/
