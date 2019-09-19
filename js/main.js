42863165c25a31a7a80ddb7310ab90a9917c38be
felipey2010 <mahamaphilip@yahoo.com>
2019-09-19 14:52:03 -0400

    second commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $('.main-slider').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
 });
 /*$('.main-slider').slick({
     infinite: true,
