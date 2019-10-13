f38d64bd057127104a866f440e9daef07de9f59b
felipey2010 <mahamaphilip@yahoo.com>
2019-10-13 03:48:24 -0400

    working on login page

---
+++ pages/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <header class="top">
+        <main>
+            <div class="header-1">
+                <!--Logo div-->
+                <div class="logo">
+                    <a href="#"><img src="./img/logo.png" style="height: 60px;" /></a>
+                </div>
+                <!--Social Media div-->
+                <div class=" social-media">
+                    <ul>
+                        <li><a href=""><img src="./img/facebook.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/twitter.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/rss.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/linkedIn.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    </header>
+    <main class="col-100 menu-urls">
+        <div class="header-login">
+            <div class="main-menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Pages</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+            <!--Searchbox-->
+            <div class="search">
+                <input type="text" placeholder="Search here">
+            </div>
+        </div>
+    </main>
+     <!--<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>-->
+     <script src="https://code.jquery.com/jquery-1.11.0.js"
+        integrity="sha256-zgND4db0iXaO7v4CLBIYHGoIIudWI5hRMQrPB20j0Qw=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+    <div class="header-2">
+        <h2>Login</h2>
+    </div>
+
+
+</body>
+<!--Fixed Footer-->
+<div class="footer">
+    <div class="text" style="float: left">
+        <p>Web Developer 2019</p>
+    </div>
+</div>
+
+</html>
---
+++ pages/register.php
@@ -0,0 +1,58 @@
+<!--<!DOCTYPE html>-->
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title>Register</title>
+</head>
+
+<body>
+    <header class="top">
+        <main>
+            <div class="header-1">
+                <!--Logo div-->
+                <div class="logo">
+                    <a href="#"><img src="./img/logo.png" style="height: 60px;" /></a>
+                </div>
+                <!--Social Media div-->
+                <div class=" social-media">
+                    <ul>
+                        <li><a href=""><img src="./img/facebook.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/twitter.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/rss.png" alt=""></a></li>
+                        <li><a href=""><img src="./img/linkedIn.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    </header>
+    <main class="col-100 menu-urls">
+        <div class="header-2">
+            <div class="main-menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Pages</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+            <!--Searchbox-->
+            <div class="search">
+                <input type="text" placeholder="Search here">
+            </div>
+        </div>
+    </main>
+    
+
+</body>
+<!--Fixed Footer-->
+<div class="footer">
+    <div class="text" style="float: left">
+        <p>Web Developer 2019</p>
+    </div>
+</div>
+
+</html>
